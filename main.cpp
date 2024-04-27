219798254d9be1daf9d5cba88a8a665cfaa343a4
JustinvdLee <vdleejustin@gmail.com>
2024-04-27 10:55:27 +0200

    moved main.cpp + changed layout

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "primeLib.h"
+#include "timingLib.h"
+
+int main() {
+    std::cout << "This is an library example\n";
+    std::cout << "If you want to use it, please have a look at the README.md!\n";
+    return 0;
+}
+
