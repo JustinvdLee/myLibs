0267db5abe71a90ac7f53f042f123849f63ba559
JustinvdLee <vdleejustin@gmail.com>
2024-04-27 13:22:37 +0200

    Added checks to see the include files correctly

--- main.cpp
+++ main.cpp
@@ -3,8 +3,16 @@
 #include "timingLib.h"
 
 int main() {
-    std::cout << "This is an library example\n";
+    std::cout << "This is an library example to show how to include the correct files\n";
     std::cout << "If you want to use it, please have a look at the README.md!\n";
+
+    Timing T;
+    T.stop();
+
+    uInt a, b;
+    int i = 11 * 19;
+    primeFactors(i, a, b);
+
     return 0;
 }
 
