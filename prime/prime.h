260981806c3998d47a1138f3db48faa24641f2f3
JustinvdLee <vdleejustin@gmail.com>
2024-05-13 23:49:43 +0200

    Revert "Readability improvents"
    This reverts commit 029e5b511de79e11d7eba04b58a829bc6c7febaf.

--- prime/prime.h
+++ prime/prime.h
@@ -1,21 +1,52 @@
 #pragma once
-#include <cstdint>
 
-/* @brief Factorise a number into two primes
- * @param n the number to factorise
- * @param &p the address of the first  prime factor
- * @param &q the address of the second prime factor
+typedef unsigned long uLong;
+typedef unsigned int  uInt ;
+typedef unsigned long long ulLong;
+
+/* Factorise a number into two primes
+ * @param n: the number to factorise
+ * &p the address of the first  prime factor
+ * &q the address of the second prime factor
  * @retval true if there were two prime factors, false if otherwise */
-bool primeFactors(const uint64_t n, uint32_t &p, uint32_t &q);
+bool primeFactors(ulLong n, uInt &p, uInt &q);
 
-/* @brief Calculate a random prime
- * @param max the maximum number that can be generated
- * @param rand a random generated number
- * @retval prime number below the max */
-uint32_t generatePrime(const uint32_t max, const uint32_t rand);
+/* Check is a number is prime 
+ * @param p: the number to check
+ * @retval: 0 if the number is a prime, else the lowest prime factor */
+constexpr uInt prime(const ulLong p) {
+    if (p <= 1)     // 0, 1 and negative numbers are not prime (defined)
+        return 1;
+    if (p <= 3 || p == 5)     // 2 and 3 are primes
+        return 0;
+    if (p % 2 == 0) // all even numbers above 3 are not prime (obviously)
+        return 2;
+    if (p % 3 == 0) // all numbers devisible by 3 above 5 are not prime 
+        return 3;
 
-/* @param Check is a number is prime 
- * @param p the number to check
- * @retval 0 if the number is a prime, else the lowest prime factor */
-constexpr uint32_t prime(const uint64_t p);
+    // if we exceed the square root of the number and haven't found a divider, the number is prime 
+    for (uInt i = 5; i*i <= p; i += 6) {
+        if (p % i == 0)
+            return i;
+        if (p % (i+2) == 0)
+            return i + 2;
+    }
+    return 0;
+}
+
+/* Generate a prime
+ * @param max: the maximum number that can be generated
+ * @retval: prime number below the max */
+constexpr uInt generatePrime(const uLong max, const uLong rand) {
+    if(max < 7)
+        return 2;
+
+    uInt number = rand % max;
+
+    if (number % 2 == 0)
+        number++;
+    while (prime(number) != 0) 
+        number -= 2;
+    return number; 
+}
 
