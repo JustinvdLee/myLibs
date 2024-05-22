cab7bcc5f767839d5bad380558e54ef9bda049e8
JustinvdLee <vdleejustin@gmail.com>
2024-05-22 11:59:26 +0200

    Added first working ringbuffer

--- app/main.cpp
+++ app/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include "prime.h"
 #include "timing.h"
+#include "ringbuffer.h"
+
+using std::cout;
+using std::endl;
 
 int main() {
-    std::cout << "This is an library example to show how to include the correct files\n";
-    std::cout << "If you want to use it, please have a look at the README.md!\n";
+    cout << "This is an example to show all the included libraries." << endl;
+    cout << "If you want to use it, please have a look at the README.md!" << endl;
 
     Timing T;
     T.stop();
@@ -12,6 +16,12 @@ int main() {
     uint32_t a, b;
     uint16_t i = 11 * 19;
     primeFactors(i, a, b);
+
+    RingBuffer<int> buf(3);
+    buf.write(1);
+    buf.write(2);
+    if (buf.read(1) != 1 || buf.read() != 2)
+        cout << "Error reading buffer!" << endl;
 
     return 0;
 }
